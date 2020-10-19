3030a8679000ec40ad0edd1bc7fdd56ceb9b1ce3
wuff <wuff@wuffs-iMac-Pro.local>
2020-10-18 23:08:11 -0500

    dinished adding auth to business endpoint
    - added get all items endpoint
    - added create items asociated to a business

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -7,8 +7,10 @@ const Auth = {
     if (!token) return res.status(400).send({ 'message': 'Not logged in' });
     try {
       const decoded = await jwt.verify(token, process.env.SECRET);
+      let query = '';
+      query = 'SELECT id_user FROM users WHERE id_user = $1;';
+      if (req.baseUrl.includes('business')) query = 'SELECT id_business FROM business WHERE id_business = $1;';
       const { userId } = decoded;
-      const query = 'SELECT id_user FROM users WHERE id_user = $1';
       const { rows } = await db.query(query, [userId]);
       if (!rows.length) return res.status(400).send({ 'message': 'Token is invalid' });
       req.user = { userId };
